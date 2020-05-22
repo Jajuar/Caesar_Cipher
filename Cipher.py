f0d1ed790574186d86a3960db4d5a209620e8acf
Jair Juarez <jjuare40@ford.com>
2020-05-22 13:20:35 -0500

    Finish Caesar Cipher ASCII

--- Cipher.py
+++ Cipher.py
@@ -14,30 +14,37 @@ message = ""
 key = ""
 msg_lst = []
 
-while (action.lower() != "-e" and action.lowe() != "-d" and action.lowe() != "-x")
-    action = input("Type option:  ")
+while (action.lower() != "-e" and action.lower() != "-d" and action.lower() != "-x"):
+    action = input("\nType option:  ")
 
-if (action.lower() == "-e"):
-    message = input("Introduce your message:\n")
+if (action.lower() == "-e" or action.lower() == "-d"):
+    message = input("\nIntroduce your message:\n\n")
     while True:
         try:
-            key = int(input("Introduce your key (Inetger from 1 to 25):  "))
+            key = int(input("\nIntroduce your key (Inetger from 1 to 25):  "))
         except:
             print("That´s not an integer ¬¬. Try again")
         else:
             if (key > 25 or key <= 0):
-                print("Please choose from 1-25 only")
+                print("\nPlease choose from 1-25 only")
             else:
                 break
-msg_lst = [chr(ord(let) + key) for let in message]
-message = "".join(msg_lst)
 
-print(f"\nYour encrypted message is:\n{message}")
-time.sleep(1)
+if (action.lower() == "-e"):
+    #Encrypt it!
+    msg_lst = [chr(ord(let) + key) for let in message]
+    message = "".join(msg_lst)
+    print(f"\nYour encrypted message is:\n\n{message}")
+    time.sleep(1)
 
 elif(action.lower() == "-d"):
-    pass
+    #Decrypt it!
+    msg_lst = [chr(ord(let) - key) for let in message]
+    message = "".join(msg_lst)
+    print(f"\nYour decrypted message is:\n\n{message}")
+    time.sleep(1)
+
 elif(action.lower() == "-x"):
-    pass
+    print("\nOkay... Bye!  :)")
 else:
     print("Sorry I don´t understand you :(")
